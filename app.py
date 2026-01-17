a5ae21d78ce248938f56ee63cc5ae885f7caff39
mohammeddaghistani <mdsa2003@gmail.com>
2026-01-18 01:54:01 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
-import streamlit as st
-from modules.db import init_db
+from modules.db import init_db, ensure_settings
 from modules.auth import login_required
 from modules.dashboard import render_dashboard
 
+
 def main():
-    st.set_page_config(page_title="نظام دعم قرار التقييم الإيجاري", page_icon="📌", layout="wide")
     init_db()
+    ensure_settings()
     user = login_required()
     render_dashboard(user)
+
 
 if __name__ == "__main__":
     main()
