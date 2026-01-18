73e18bb25a374fc4d53b148ed75fca0326fab522
mohammeddaghistani <mdsa2003@gmail.com>
2026-01-18 12:03:17 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,23 +1,13 @@
 import streamlit as st
-
-# يجب أن يكون هذا أول نداء لـ Streamlit بعد الاستيراد مباشرة
-st.set_page_config(
-    page_title="تقدير القيمة الإيجارية للعقارات الاستثمارية",
-    page_icon="🏢",
-    layout="wide",
-    initial_sidebar_state="expanded",
-)
-
 from modules.db import init_db
 from modules.auth import login_required
 from modules.dashboard import render_dashboard
 
-
 def main():
+    st.set_page_config(page_title="نظام دعم قرار التقييم الإيجاري", page_icon="📌", layout="wide")
     init_db()
     user = login_required()
     render_dashboard(user)
-
 
 if __name__ == "__main__":
     main()
